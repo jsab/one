fc17a87a5ebc7fc2c789b10ff753b60844964a6a
Jean-Sebastien A. Beaudry <git@jsab.me>
2014-01-02 08:58:51 -0500

    Added init and exit method for module.

--- one.c
+++ one.c
@@ -2,6 +2,25 @@
    opposite of zero. */
 
 #include <linux/module.h>    /* included for all kernel modules */
+#include <linux/kernel.h>    /* included for KERN_INFO */
+#include <linux/init.h>      /* included for __init and __exit macros */
+
+
+static int __init one_init(void)
+{
+    printk(KERN_INFO "one: Module initialization!");
+    return 0;    /* Zero = success */
+}
+
+
+static void __exit one_exit(void)
+{
+    printk(KERN_INFO "one: Module clean-up and exit!");
+}
+
+
+module_init(one_init);
+module_exit(one_exit);
 
 MODULE_AUTHOR("Jean-Sebastien A. Beaudry");
 MODULE_DESCRIPTION("Not /dev/zero");
